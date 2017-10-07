4c1dbed00342cae169449c332466f3a764e4d128
Shakib034 <sk779866124@gmail.com>
2017-10-07 18:26:53 +0600

    added all the file

--- app/src/main/java/com/shakibcsekuet/mainbooklistproject/HomeActivity.java
+++ app/src/main/java/com/shakibcsekuet/mainbooklistproject/HomeActivity.java
@@ -5,190 +5,77 @@ package com.shakibcsekuet.mainbooklistproject;
  */
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
 import android.view.View;
-import android.widget.ExpandableListView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;import android.widget.ListView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
+public class HomeActivity extends Activity
+{
+    // Array of strings...
+    ListView simpleList;
+    String countryList[] = {"CSE","EEE","MECANICAL","BME","ECE","IEM","BECM","TE","ET","LE","URP"};
 
-public class HomeActivity extends AppCompatActivity {
-    ///--------//////////////////////////////////--------////////////
-    private LinkedHashMap<String, GroupInfo> subjects = new LinkedHashMap<String, GroupInfo>();
-    private ArrayList<GroupInfo> deptList = new ArrayList<GroupInfo>();
-
-    private CustomAdapter listAdapter;
-    private ExpandableListView simpleExpandableListView;
-    ///--------//////////////////////////////////--------////////////
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-        ///--------//////////////////////////////////--------////////////
-
-        loadData();
-
-
-        simpleExpandableListView = (ExpandableListView) findViewById(R.id.simpleExpandableListView);
-
-        listAdapter = new CustomAdapter(HomeActivity.this, deptList);
-
-        simpleExpandableListView.setAdapter(listAdapter);
-
-
-        expandAll();
-
-
-        simpleExpandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+    @Override   protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);      setContentView(R.layout.activity_home);
+        simpleList = (ListView)findViewById(R.id.simpleListView);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_home_listview, R.id.textView, countryList);
+        simpleList.setAdapter(arrayAdapter);
+        simpleList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
-
-                GroupInfo headerInfo = deptList.get(groupPosition);
-
-                ChildInfo detailInfo =  headerInfo.getProductList().get(childPosition);
-
-                Toast.makeText(getBaseContext(), " Clicked on :: " + headerInfo.getName()
-                        + "/" + detailInfo.getName(), Toast.LENGTH_LONG).show();
-                if(headerInfo.getName()=="KUET" && detailInfo.getName()=="CSE"){
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), " Clicked on :: " +countryList[position], Toast.LENGTH_LONG).show();
+                if(countryList[position]=="CSE"){
                     Intent intent = new Intent(getApplicationContext(), CSE.class);
                     startActivity(intent);
                 }
-                else if(headerInfo.getName()=="KUET" && detailInfo.getName()=="EEE"){
+                else if(countryList[position]=="EEE"){
                     Intent intent = new Intent(getApplicationContext(), EEE.class);
                     startActivity(intent);
                 }
-                else if(headerInfo.getName()=="KUET" && detailInfo.getName()=="MECANICAL"){
+                else if(countryList[position]=="MECANICAL"){
                     Intent intent = new Intent(getApplicationContext(), MECA.class);
                     startActivity(intent);
                 }
-                else if(headerInfo.getName()=="KUET" && detailInfo.getName()=="CIVIL"){
+                else if(countryList[position]=="CIVIL"){
                     Intent intent = new Intent(getApplicationContext(), CIVIL.class);
                     startActivity(intent);
                 }
-                else if(headerInfo.getName()=="KUET" && detailInfo.getName()=="BME"){
+                else if(countryList[position]=="BME"){
                     Intent intent = new Intent(getApplicationContext(), BME.class);
                     startActivity(intent);
                 }
-                else if(headerInfo.getName()=="KUET" && detailInfo.getName()=="ECE"){
+                else if(countryList[position]=="ECE"){
                     Intent intent = new Intent(getApplicationContext(), ECE.class);
                     startActivity(intent);
                 }
-                else if(headerInfo.getName()=="KUET" && detailInfo.getName()=="IEM"){
+                else if(countryList[position]=="IEM"){
                     Intent intent = new Intent(getApplicationContext(), IEM.class);
                     startActivity(intent);
                 }
-                else if(headerInfo.getName()=="KUET" && detailInfo.getName()=="BECM"){
+                else if(countryList[position]=="BECM"){
                     Intent intent = new Intent(getApplicationContext(), BECM.class);
                     startActivity(intent);
                 }
-                else if(headerInfo.getName()=="KUET" && detailInfo.getName()=="TE"){
+                else if(countryList[position]=="TE"){
                     Intent intent = new Intent(getApplicationContext(), TE.class);
                     startActivity(intent);
                 }
-                else if(headerInfo.getName()=="KUET" && detailInfo.getName()=="ET"){
+                else if(countryList[position]=="ET"){
                     Intent intent = new Intent(getApplicationContext(), ET.class);
                     startActivity(intent);
                 }
-                else if(headerInfo.getName()=="KUET" && detailInfo.getName()=="LE"){
+                else if(countryList[position]=="LE"){
                     Intent intent = new Intent(getApplicationContext(), LE.class);
                     startActivity(intent);
                 }
-                else if(headerInfo.getName()=="KUET" && detailInfo.getName()=="URP"){
+                else if(countryList[position]=="URP"){
                     Intent intent = new Intent(getApplicationContext(), URP.class);
                     startActivity(intent);
                 }
-
-
-
-
-
-
-
-                return false;
             }
         });
-
-        simpleExpandableListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
-            @Override
-            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
-
-                GroupInfo headerInfo = deptList.get(groupPosition);
-
-                Toast.makeText(getBaseContext(), " Header is :: " + headerInfo.getName(),
-                        Toast.LENGTH_LONG).show();
-
-                return false;
-            }
-        });
-
-
-
-        ///--------//////////////////////////////////--------////////////
     }
-    ///--------//////////////////////////////////--------////////////
-
-    private void expandAll() {
-        int count = listAdapter.getGroupCount();
-        for (int i = 0; i < count; i++){
-            simpleExpandableListView.expandGroup(i);
-        }
-    }
-
-
-    private void collapseAll() {
-        int count = listAdapter.getGroupCount();
-        for (int i = 0; i < count; i++){
-            simpleExpandableListView.collapseGroup(i);
-        }
-    }
-
-
-    private void loadData(){
-
-        addProduct("KUET","CSE");
-        addProduct("KUET","EEE");
-        addProduct("KUET","MECANICAL");
-        addProduct("KUET","CIVIL");
-        addProduct("KUET","BME");
-        addProduct("KUET","ECE");
-        addProduct("KUET","IEM");
-        addProduct("KUET","BECM");
-        addProduct("KUET","TE");
-        addProduct("KUET","ET");
-        addProduct("KUET","LE");
-        addProduct("KUET","URP");
-    }
-    private int addProduct(String department, String product){
-
-        int groupPosition = 0;
-
-
-        GroupInfo headerInfo = subjects.get(department);
-
-        if(headerInfo == null){
-            headerInfo = new GroupInfo();
-            headerInfo.setName(department);
-            subjects.put(department, headerInfo);
-            deptList.add(headerInfo);
-        }
-
-
-        ArrayList<ChildInfo> productList = headerInfo.getProductList();
-        int listSize = productList.size();
-        listSize++;
-
-        ChildInfo detailInfo = new ChildInfo();
-        detailInfo.setSequence(String.valueOf(listSize));
-        detailInfo.setName(product);
-        productList.add(detailInfo);
-        headerInfo.setProductList(productList);
-
-
-        groupPosition = deptList.indexOf(headerInfo);
-        return groupPosition;
-    }
-    ///--------//////////////////////////////////--------////////////
 }
